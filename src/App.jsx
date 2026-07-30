e341ce7a8dd989d7132b053de4b6c3886ec38a47
INCOGNITO <adeleyedara4@gmail.com>
2026-07-30 13:12:02 -0700

    Fix Vercel React routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { useEffect, useState } from "react";
 
 import Home from "./pages/Home";
 import Workspace from "./pages/Workspace";
@@ -7,93 +6,11 @@ import SavedProjects from "./pages/SavedProjects";
 import AIChat from "./pages/AIChat";
 import FormulaReference from "./pages/FormulaReference";
 import Projects from "./pages/Projects";
-
+import { useState } from "react";
 import Splash from "./components/Splash";
-
-import {
-  setupInstallPrompt,
-  showInstallPrompt,
-} from "./installPrompt";
-
 function App() {
-  const [showSplash, setShowSplash] = useState(true);
-  const [canInstall, setCanInstall] = useState(false);
-
-  useEffect(() => {
-    setupInstallPrompt();
-
-    const handleInstallAvailable = () => {
-      setCanInstall(true);
-    };
-
-    const handleInstalled = () => {
-      setCanInstall(false);
-    };
-
-    window.addEventListener(
-      "incog-install-available",
-      handleInstallAvailable
-    );
-
-    window.addEventListener(
-      "incog-app-installed",
-      handleInstalled
-    );
-
-    return () => {
-      window.removeEventListener(
-        "incog-install-available",
-        handleInstallAvailable
-      );
-
-      window.removeEventListener(
-        "incog-app-installed",
-        handleInstalled
-      );
-    };
-  }, []);
-
-  if (showSplash) {
-    return (
-      <Splash
-        onFinish={() => setShowSplash(false)}
-      />
-    );
-  }
-
   return (
     <BrowserRouter>
-
-      {canInstall && (
-        <button
-          onClick={async () => {
-            const installed = await showInstallPrompt();
-
-            if (installed) {
-              setCanInstall(false);
-            }
-          }}
-          className="
-            fixed
-            bottom-5
-            right-5
-            z-[9999]
-            rounded-xl
-            bg-blue-600
-            px-5
-            py-3
-            font-semibold
-            text-white
-            shadow-xl
-            transition
-            hover:bg-blue-700
-            active:scale-95
-          "
-        >
-          Install INCOG Maths
-        </button>
-      )}
-
       <Routes>
 
         <Route
@@ -115,21 +32,23 @@ function App() {
           path="/ai"
           element={<AIChat />}
         />
-
-        <Route
-          path="/reference"
-          element={<FormulaReference />}
-        />
-
-        <Route
-          path="/projects/all"
-          element={<Projects />}
-        />
-
+         <Route
+  path="/reference"
+  element={<FormulaReference />}
+/> 
+         <Route path="/projects" element={<Projects />} />
       </Routes>
-
     </BrowserRouter>
   );
 }
 
 export default App;
+const [showSplash, setShowSplash] = useState(true);
+
+if (showSplash) {
+  return (
+    <Splash 
+      onFinish={() => setShowSplash(false)}
+    />
+  );
+}
